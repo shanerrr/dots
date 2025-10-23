47219f450ea0806b5471976e6824670e266eadaa
Shane <theshanukg@gmail.com>
2025-10-22 22:04:13 -0600

    nvim start and kanata service

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+  -- change some telescope options and a keymap to browse plugin files
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+      {
+        "<leader>fg",
+        function() require("telescope.builtin").live_grep({ cwd = require("lazy.core.config").options.root }) end,
+      }
+    },
+    -- change some options
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+  },
+}
