804c9101a05cd29d45bffdd7fee05cf3182df4bf
Shane <theshanukg@gmail.com>
2025-12-02 23:38:16 -0700

    started using snacks explorer over nvim tree

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,10 +1,34 @@
 return {
   {
-    "akinsho/bufferline.nvim",
-    opts = function(_, opts)
-      if (vim.g.colors_name or ""):find("catppuccin") then
-        opts.highlights = require("catppuccin.special.bufferline").get_theme()
+  "akinsho/bufferline.nvim",
+  opts = function()
+    local Offset = require("bufferline.offset")
+    if not Offset.edgy then
+      local get = Offset.get
+      Offset.get = function()
+        if package.loaded.edgy then
+          local old_offset = get()
+          local layout = require("edgy.config").layout
+          local ret = { left = "", left_size = 0, right = "", right_size = 0 }
+          for _, pos in ipairs({ "left", "right" }) do
+            local sb = layout[pos]
+            local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
+            if sb and #sb.wins > 0 then
+              ret[pos] = old_offset[pos .. "_size"] > 0 and old_offset[pos]
+                or pos == "left" and ("%#Bold#" .. title .. "%*" .. "%#BufferLineOffsetSeparator#│%*")
+                or pos == "right" and ("%#BufferLineOffsetSeparator#│%*" .. "%#Bold#" .. title .. "%*")
+              ret[pos .. "_size"] = old_offset[pos .. "_size"] > 0 and old_offset[pos .. "_size"] or sb.bounds.width
+            end
+          end
+          ret.total_size = ret.left_size + ret.right_size
+          if ret.total_size > 0 then
+            return ret
+          end
+        end
+        return get()
       end
-    end,
-  }
+      Offset.edgy = true
+    end
+  end,
+}
 }
---
+++ .config/nvim/lua/plugins/qol.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      explorer = {enabled = true },
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+     keys = {
+      {
+        '<C-b>',
+        function()
+          local explorer_win = nil
+
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            local ft = vim.bo[buf].filetype
+            if ft == 'snacks_picker_list' then
+              explorer_win = win
+              break
+            end
+          end
+
+          if vim.api.nvim_get_current_win() ~= explorer_win and explorer_win then
+            vim.api.nvim_set_current_win(explorer_win)
+          else
+            Snacks.explorer()
+          end
+        end,
+        desc = 'Snacks File Explorer',
+
+      },     
+    },
+
+  }
+}
